c59b737681f2f462f08545ff69cc66f81c3ac058
Robin <robin.frodl@student.uni-tuebingen.de>
2024-12-16 15:00:52 +0100

    Matrix colorscheme added

--- lua/frodo/init.lua
+++ lua/frodo/init.lua
@@ -4,4 +4,6 @@ require("frodo.remap")
 require("frodo.lazy_init")
 
 
-vim.cmd [[colorscheme substrata]]
+-- vim.cmd [[colorscheme substrata]]
+-- vim.cmd [[colorscheme matrix]]
+vim.cmd [[colorscheme default]]
