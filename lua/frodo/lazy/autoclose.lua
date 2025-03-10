3bc6a9e8372208fbe998d2d77795b60b76dd9123
NoRacket <ronmaecus@gmail.com>
2025-03-10 10:24:22 +0100

    added some goto definition feature

--- lua/frodo/lazy/autoclose.lua
+++ lua/frodo/lazy/autoclose.lua
@@ -5,9 +5,13 @@ return {
     config = function()
         require("autoclose").setup({
             keys = {
-                ["("] = { escape = false, close = true, pair = "()" },
-                ["["] = { escape = false, close = true, pair = "[]" },
-                ["{"] = { escape = false, close = true, pair = "{}" },
+                -- ["("] = { escape = false, close = true, pair = "()" },
+                -- ["["] = { escape = false, close = true, pair = "[]" },
+                -- ["{"] = { escape = false, close = true, pair = "{}" },
+
+                ["("] = { escape = false, close = false, pair = "()" },
+                ["["] = { escape = false, close = false, pair = "[]" },
+                ["{"] = { escape = false, close = false, pair = "{}" },
 
                 ["<"] = { escape = false, close = false, pair = "<>" },
                 [">"] = { escape = false, close = false, pair = "<>" },
--- lua/frodo/remap.lua
+++ lua/frodo/remap.lua
@@ -7,8 +7,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>M")
 vim.keymap.set("n", "<C-u>", "<C-u>M")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-f>", "<C-f>M")
+vim.keymap.set("n", "<C-b>", "<C-b>M")
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
@@ -23,28 +27,34 @@ vim.keymap.set("n", "Q", "<nop>")
 --vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>o", "<cmd>copen<CR><C-w><C-w>")
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>o", "<cmd>copen<CR><C-w><C-w>") -- open the error log
+vim.keymap.set("n", "<leader>q", "<cmd>cclose<CR>")          -- close the error log
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")             -- walk through \
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")             -- error log
+-- vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gcI<Left><Left><Left><Left>]])
+vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gcI<Left><Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- vim.keymap.set("n", "<leader><leader>", function()
---     vim.cmd("so")
--- end)
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
 
+-- reset the highlight after searching
 vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch)
 
+-- movement and insertion
 vim.keymap.set("i", "<C-l>", "<right>")
 vim.keymap.set("i", "<C-j>", "<Esc>o")
 vim.keymap.set("i", "<C-k>", "<Esc>O")
+vim.keymap.set("i", "<C-h>", "<left>")
+
+-- open files the old way (if you konow the name) / run through buffers.
 vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
 vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
 vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
-vim.keymap.set("n", "<leader>be", ":e %:h/")
+vim.keymap.set("n", "<leader>bb", ":e %:h/")
 -- vim.keymap.set("i", "{<Cr>", "{<Cr>}<Esc>O<Tab>")
 
--- vim.keymap.set("n", "<C-h>", "<cmd>make %<CR><cmd>copen<CR><C-w><C-w>")
+vim.keymap.set("n", "<leader>m", "<cmd>:w<CR><cmd>make %<CR><cmd>copen<CR><C-w><C-w>")
